643720d1cd3b3e41a9ec4cacd070f76c77160728
nomad <nomad@nomad.nomad>
2026-07-29 00:46:23 -0400

    adds code for TxtOrderAdapter

--- concrete/src/main/java/org/example/orderapp/adapter/impl/TxtOrderAdapter.java
+++ concrete/src/main/java/org/example/orderapp/adapter/impl/TxtOrderAdapter.java
@@ -1,6 +1,31 @@
 package org.example.orderapp.adapter.impl;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-// public class TxtOrderAdapter implements OrderAdapter {
+import org.example.orderapp.adapter.OrderSource;
+import org.example.orderapp.model.Order;
+import org.example.orderapp.parser.impl.OrderParserImpl;
+import org.example.orderapp.utils.FileReader;
 
-// }
+public class TxtOrderAdapter implements OrderSource {
+    private final OrderParserImpl parser = new OrderParserImpl();
+    private final FileReader fileReader = new FileReader();
+    private final String datapath = "concrete/data/inbound_files/discount_day.txt";
+
+
+    @Override
+    public List<Order> read() throws IOException {
+        List<Order> orders = new ArrayList<>();
+        List<String> lines = fileReader.readFileLineByLine(datapath);
+
+        for (String line : lines) {
+            line = line.replace("|", ";");
+            orders.add(parser.parseLinesToOrder(line));
+        }
+
+        return orders;
+    }
+
+}
