e343f8483a7e32dbf12f4b6c9da23c489a4c5816
nomad <nomad@nomad.nomad>
2026-08-06 01:18:05 -0400

    changes the logic in OrderPriceManager

--- concrete/src/main/java/org/example/orderapp/model/Receipt.java
+++ concrete/src/main/java/org/example/orderapp/model/Receipt.java
@@ -2,17 +2,11 @@ package org.example.orderapp.model;
 
 import java.time.LocalDateTime;
 
-
-
 public class Receipt {
     private LocalDateTime receiptDateTime;
     private String customerName;
     private double productAmount;
     private double totalCost;
-
-    
-
-    
 
     public Receipt(LocalDateTime receiptDateTime, String customerName, double productAmount, double totalCost) {
         this.receiptDateTime = receiptDateTime;
@@ -21,65 +15,42 @@ public class Receipt {
         this.totalCost = totalCost;
     }
 
-
-
     @Override
     public String toString() {
-        return "Receipt [getTotalCost()=" + getTotalCost() + ", getReceiptDateTime()=" + getReceiptDateTime()
-                + ", getCustomerName()=" + getCustomerName() + ", getProductAmount()=" + getProductAmount() + "]";
+        return "Receipt" + " " + getReceiptDateTime()
+                + " " + getCustomerName() + " " + getProductAmount() + " " + getTotalCost();
     }
-
-
 
     public LocalDateTime getReceiptDateTime() {
         return receiptDateTime;
     }
 
-
-
     public void setReceiptDateTime(LocalDateTime receiptDateTime) {
         this.receiptDateTime = receiptDateTime;
     }
-
-
 
     public String getCustomerName() {
         return customerName;
     }
 
-
-
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
     }
-
-
 
     public double getProductAmount() {
         return productAmount;
     }
 
-
-
     public void setProductAmount(double productAmount) {
         this.productAmount = productAmount;
     }
-
-
 
     public double getTotalCost() {
         return totalCost;
     }
 
-
-
     public void setTotalCost(double totalCost) {
         this.totalCost = totalCost;
     }
 
-
-
-
-    
-    
 }
--- concrete/src/main/java/org/example/orderapp/service/OrderPriceManager.java
+++ concrete/src/main/java/org/example/orderapp/service/OrderPriceManager.java
@@ -8,41 +8,6 @@ import org.example.orderapp.model.Receipt;
 
 public class OrderPriceManager {
 
-    // public List<Receipt> checkoutOrder(List<Order> orders, double productPrice,
-    // double productPriceDiscount,
-    // double discountStepdown) {
-
-    // List<Receipt> noDiscountReceipts = new ArrayList<>();
-
-    // for (Order order : orders) {
-
-    // double orderCost = order.getProductAmount() * productPrice;
-
-    // noDiscountReceipts.add(new Receipt(order.getOrderDateTime(),
-    // order.getCustomerName(),
-    // order.getProductAmount(),
-    // orderCost));
-    // }
-
-    // double currentDiscount = productPriceDiscount / 100;
-    // List<Receipt> receipts = new ArrayList<>();
-
-    // for (int i = 0; i < noDiscountReceipts.size(); i++) {
-    // Receipt receipt = noDiscountReceipts.get(i);
-
-    // double orderCost = Math.round(receipt.getTotalCost() * (1 -
-    // currentDiscount));
-
-    // // stepdown
-    // currentDiscount = Math.max(0, currentDiscount - discountStepdown / 100);
-
-    // receipts.add(new Receipt(receipt.getReceiptDateTime(),
-    // receipt.getCustomerName(),
-    // receipt.getProductAmount(), orderCost));
-    // }
-    // return receipts;
-    // }
-
     public List<Receipt> checkoutOrder(
             List<Order> orders,
             double productPrice,
@@ -67,7 +32,7 @@ public class OrderPriceManager {
                     0,
                     currentDiscount - discountStepdown / 100.0);
         }
-
+        receipts.forEach(System.out::println);
         return receipts;
     }
 
